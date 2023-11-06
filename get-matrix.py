f5849202b8d90d2482dccac83ac9e0b9c608c8ae
Tobias Gruetzmacher <tobias-git@23.gs>
2023-11-07 00:15:19 +0100

    Initial version

---
+++ get-matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import time
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+target = Path(__file__).parent / 'pages'
+target.mkdir(exist_ok=True)
+tfile = target / 'product_matrix.csv'
+tfile.unlink(missing_ok=True)
+
+options = webdriver.ChromeOptions()
+#options.headless = True
+options.add_experimental_option('prefs', {
+    "download.default_directory": str(target),
+})
+driver = webdriver.Chrome(options=options)
+driver.get('https://mikrotik.com/products/matrix')
+csv_link = driver.find_element(by=By.CSS_SELECTOR, value="a.csv")
+csv_link.click()
+while not tfile.exists():
+    time.sleep(0.1)
+driver.quit()
